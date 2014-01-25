41b4358e376ad3f77a87efc96b5ec21ea5e2d99e
annisa <miegoreng10@gmail.com>
2014-01-25 18:16:32 +0700

    masih mencoba

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  printf("\nNIM             : 10112534\n");
+  printf("Nama            : Annisa Amelia\n");
+  printf("Jenis Kelamin   : Wanita\n");
+  printf("Pekerjaan       : Mahasiswa\n");
+  printf("Alamat          : Jl. Ir. H. Djuanda BLK 220 Simpang Dago\n");
+  printf("Email Facebook  : annicaamelia@yahoo.com\n");
+  printf("No Hp           : 085786834372\n");
+  printf("\n\n\n");
+  system("PAUSE");	
+  return 0;
+}
